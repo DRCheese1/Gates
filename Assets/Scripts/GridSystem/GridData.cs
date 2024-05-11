3ecdc3c0163887a96fb6b44a3daa9697c01438f6
DR Cheese <emil.Fester@hotmail.com>
2024-05-11 10:19:35 +0200

    Implemented object clearance system

--- Assets/Scripts/GridSystem/GridData.cs
+++ Assets/Scripts/GridSystem/GridData.cs
@@ -10,13 +10,41 @@ public class GridData
     public void AddObject(Vector3Int gridPosition, Vector2Int objectSize, int Id, int selectedObjectId)
     {
         List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
+        PlacementData data = new PlacementData(positionToOccupy, Id, selectedObjectId);
+
+        foreach (var pos in positionToOccupy)
+        {
+            if(placedObjects.ContainsKey(pos))
+                    throw new Exception($"Slot occupied {pos}");
+            placedObjects[pos] = data;
+        }
     }
 
     private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize)
     {
-        throw new NotImplementedException();
+        List<Vector3Int> returnVal = new();
+        for (int x = 0; x < objectSize.x; x++)
+        {
+            for (int y = 0; y < objectSize.y; y++)
+            {
+                returnVal.Add(gridPosition + new Vector3Int(x, 0, y));
+            }
+        }
+        return returnVal;
+    }
+
+    public bool Blocked(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        List<Vector3Int> positionsToOccupy = CalculatePositions(gridPosition, objectSize);
+        foreach (var pos in positionsToOccupy)
+        {
+            if(placedObjects.ContainsKey(pos))
+                return true;
+        }
+        return false;
     }
 }
+
 
 public class PlacementData
 {
--- Assets/Scripts/GridSystem/GridPlacementSystem.cs
+++ Assets/Scripts/GridSystem/GridPlacementSystem.cs
@@ -6,21 +6,27 @@ using UnityEngine;
 
 public class GridPlacementSystem : MonoBehaviour
 {
+    //Used for multiple things:
     [SerializeField] private MouseManger mouseManger; //Lets you acces the mousePositionDetector;
+    private Renderer previewRenderer; //Used to render the gridIndicator in different ways
 
+    //Used for Main object placement:
     [SerializeField] private ObjectListSO objectList; //Allows you to acces the list of objects
+    [SerializeField] private GridData floorData, objectData; //Used for storing and checking grid datapr
+    private List<GameObject> placedGameObjects = new();
     private int selectedObjectId = -1; //Id of selected object
 
+    //Used for main grid worings:
     [SerializeField] private Grid grid; //Lets you acces the Grid component
     [SerializeField] private GameObject mouseIndicator, cellIndicator; //Shows where you are selecting
     [SerializeField] private GameObject GridVis; //Refrences to the grid visualization
 
-    [SerializeField] Vector3Int debug1;
-    [SerializeField] Vector3Int debug2;
-
     private void Start() //Unity start
     {
-        StopPlacement();
+        StopPlacement(); //Stops placement
+        floorData = new();
+        objectData = new();
+        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
     }
 
     public void StartPlacement(int Id) //Starts placement and shows grid
@@ -46,9 +52,23 @@ public class GridPlacementSystem : MonoBehaviour
 
         Vector3 mousePosition = mouseManger.GetMousePosition(); //Gets the mousePosition from mousePositionDetector script
         Vector3Int gridPosition = grid.WorldToCell(mousePosition); //Rounds mouseposition to nearest grid cell
-        debug1 = gridPosition;
+
+        bool canPlace = canPlaceTest(gridPosition, selectedObjectId);
+        if(canPlace == false)
+            return;
+
         GameObject newObject = Instantiate(objectList.GridObjectData[selectedObjectId].Prefab); //Creates the currently selected prefab
         newObject.transform.position = grid.CellToWorld(gridPosition); //Sets the newObject to the current grid cell
+        placedGameObjects.Add(newObject);
+        GridData selectedData = objectList.GridObjectData[selectedObjectId].Id == 0 ? floorData : objectData;
+        selectedData.AddObject(gridPosition, objectList.GridObjectData[selectedObjectId].Size, objectList.GridObjectData[selectedObjectId].Id, placedGameObjects.Count -1);
+
+    }
+
+    private bool canPlaceTest(Vector3Int gridPosition, int selectedObjectId)
+    {
+        GridData selectedData = objectList.GridObjectData[selectedObjectId].Id == 0 ? floorData : objectData;
+        return !selectedData.Blocked(gridPosition, objectList.GridObjectData[selectedObjectId].Size);
     }
 
     private void StopPlacement()
@@ -66,8 +86,12 @@ public class GridPlacementSystem : MonoBehaviour
             return; //Returns
         Vector3 mousePosition = mouseManger.GetMousePosition(); //Gets the mousePosition from mousePossitionDetector script
         Vector3Int gridPosition = grid.WorldToCell(mousePosition); //Rounds mouseposition to nearest grid cell
-        debug2 = gridPosition;
+
+        bool canPlace = canPlaceTest(gridPosition, selectedObjectId);
+        previewRenderer.material.color = canPlace ? Color.white : Color.red;
+
         mouseIndicator.transform.position = mousePosition; //Sets mouseindicator to mouseposition
-        cellIndicator.transform.position = grid.CellToWorld(gridPosition); //Sets the gridIndicator to the current grid cell
+        Vector3 pos = grid.CellToWorld(gridPosition); //Calculates the position of the cellindicator
+        pos.y = 0.01f; cellIndicator.transform.position = pos; //Sets the gridIndicator to the current grid cell
     }
 }
